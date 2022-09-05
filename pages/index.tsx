a548affe254b383e1d3449d83818ee396a7288e2
GaPolveiro02 <gabrielpolveiro12@gmail.com>
2022-09-05 20:31:59 -0300

    Adding external font and dynamic title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,12 @@
+import Head from "../node_modules/next/head"
+
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>Início | ig.news</title>
+    </Head>
     <h1>Hello World!</h1>
+    </>
   )
 }
