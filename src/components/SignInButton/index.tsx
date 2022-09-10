7ee19af29ba0184e351d6430044c2152a54123ab
GaPolveiro02 <gabrielpolveiro12@gmail.com>
2022-09-10 17:25:59 -0300

    Adding SignInButton Component

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,28 @@
+import { FaGithub } from "../../../node_modules/react-icons/fa/index";
+import { FiX } from "../../../node_modules/react-icons/fi/index";
+
+
+import styles from './styles.module.scss'
+
+export function SignInButton() {
+    const isUserLoggedIn = true;
+
+    return isUserLoggedIn ? (
+        <button 
+            type="button"
+            className={styles.signInButton}
+        >
+            <FaGithub color="#04d361"/>
+            UserName
+            <FiX color="#737380" className={styles.closeIcon}/>
+        </button>
+    ) : (
+        <button 
+            type="button"
+            className={styles.signInButton}
+        >
+            <FaGithub color="#eba417"/>
+            Sign in with Github
+        </button>
+    )
+}
